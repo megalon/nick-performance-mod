d62b5d20601e2a2a73ad8888b146ad5a1cdddd8f
Megalon <megalongaming@gmail.com>
2021-12-29 14:56:59 -0800

    Basic mod boilerplate

---
+++ NickPerformanceMod/PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NickPerformanceMod
+{
+    class PluginInfo
+    {
+        public const string PLUGIN_GUID = "megalon.nick_performance_mod";
+        public const string PLUGIN_NAME = "Nick Performance Mod";
+        public const string PLUGIN_VERSION = "0.1.0";
+    }
+}
